ceb667a9e3e0d6ce74e5c379d0a4719a9fcd89f4
Bryan Vega <bdvegat@unal.edu.co>
2019-05-17 22:19:36 -0500

    seeds file: generating users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+#crear 50 usuarios
+100.times do |row|
+    User.create( nombre: Faker::Name.first_name,
+    id: Faker::Number(10), apellidos: Faker::Name.last_name,
+    direccion: Faker::Address.street_address, telefono: Faker::Number(10))
+end
+
