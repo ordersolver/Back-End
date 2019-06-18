efbdb138b768482f37970fa98f868cb581d57392
Julio Ovalle <jcovallel@unal.edu.co>
2019-06-18 18:40:05 -0500

    correo2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,4 +43,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+  # Mailer configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => "danicas97h@gmail.com",
+      :password             => "Combino2",
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
 end
