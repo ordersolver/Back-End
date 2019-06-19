318c5a86eb3a41110987f57455886b64d3b39f12
Julio Ovalle <jcovallel@unal.edu.co>
2019-06-18 19:36:08 -0500

    mail4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,14 +83,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Mailer configuration
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :user_name            => "danicas97h@gmail.com",
-      :password             => "Combino2",
-      :authentication       => "plain",
-      :enable_starttls_auto => true
-  }
 end
